3b0de3c73d106bec5efc3074b433010ca15a8093
Marnix Kraus <marnix@praxasense.com>
2023-02-24 11:34:58 +0100

    export all relevant types

--- lib/edflib.dart
+++ lib/edflib.dart
@@ -7,5 +7,9 @@
 /// TODO: Some info about how to use this library. And something about the ENV variable to the library itself.
 library dartedflib;
 
+export 'src/annotations_mode.dart';
+export 'src/edf_error.dart';
+export 'src/edf_reader.dart';
 export 'src/edf_writer.dart';
+export 'src/file_type.dart';
 export 'src/version.dart';
